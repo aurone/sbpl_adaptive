4ada635509e108cdbb325044b2c0c89f761cb892
karthik <karthik4294@gmail.com>
2016-08-13 21:20:23 -0400

    Add mha_adaptive planner

--- sbpl_adaptive/include/sbpl_adaptive/headers.h
+++ sbpl_adaptive/include/sbpl_adaptive/headers.h
@@ -58,6 +58,7 @@
 
 #include <sbpl_adaptive/planners/TRAStar/traplanner.h>
 #include <sbpl_adaptive/planners/AdaptivePlanner/araplanner_ad.h>
+#include <sbpl_adaptive/planners/AdaptivePlanner/mhaplanner_ad.h>
 #include <sbpl_adaptive/planners/AdaptivePlanner/adaptive_planner.h>
 
 #endif
