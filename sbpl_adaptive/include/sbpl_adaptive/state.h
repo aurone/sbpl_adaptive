2dd805b0edf83f71f25852059d2721b3c37d7725
Andrew Dornbush <andrew.dornbush@gmail.com>
2017-02-24 14:08:20 -0500

    give state-specific data a bona-fide type to do type checking in debug mode

--- sbpl_adaptive/include/sbpl_adaptive/state.h
+++ sbpl_adaptive/include/sbpl_adaptive/state.h
@@ -9,22 +9,53 @@
 #define SBPL_ADAPTIVE_STATE_H
 
 #include <stdlib.h>
+#include <stdexcept>
 
 namespace adim {
 
+#define STATE_CAST_DEBUG 1
+
+struct AdaptiveState
+{
+#if STATE_CAST_DEBUG
+  // virtual destructor to force dynamic type information
+  virtual ~AdaptiveState() { }
+#endif
+};
+
 struct AdaptiveHashEntry
 {
-    size_t stateID;         // the state ID
-    size_t heur;            // the heuristic value
-    char dimID;             // the dimensionality ID
-    void *stateData;        // the state data specific to this dimensionality
+    size_t stateID;           // the state ID
+    size_t heur;              // the heuristic value
+    char dimID;               // the dimensionality ID
+    AdaptiveState *stateData; // the state data specific to this dimensionality
 
-    template <class T> T *dataAs() {
+    template <class T>
+    T *dataAs()
+    {
+#if STATE_CAST_DEBUG
+        T *state = dynamic_cast<T *>(stateData);
+        if (!state) {
+          throw std::runtime_error("bad cast");
+        }
+        return state;
+#else
         return static_cast<T *>(stateData);
+#endif
     }
 
-    template <class T> const T *dataAs() const {
+    template <class T>
+    const T *dataAs() const
+    {
+#if STATE_CAST_DEBUG
+        const T *state = dynamic_cast<const T *>(stateData);
+        if (!state) {
+          throw std::runtime_error("bad cast");
+        }
+        return state;
+#else
         return static_cast<const T *>(stateData);
+#endif
     }
 };
 
